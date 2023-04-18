43dbdf611ecad26d3880ea9c7eae437cc98e300a
anushaa111 <86736177+anushapahariya@users.noreply.github.com>
2023-04-18 11:34:30 +0530

    add updates

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ const App = () => (
   <div>
     <Navbar />
      <Header />
-   {/* <AboutUs />
+    <AboutUs />
     <SpecialMenu />
     <Chef />
     <Intro />
     <Laurels />
     <Gallery />
     <FindUs />
-    <Footer /> */}
+    <Footer /> 
   </div>
 );
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import { MdOutlineRestaurantMenu } from 'react-icons/md'; 
 import images from '../../constants/images';
 import './Navbar.css';
 
